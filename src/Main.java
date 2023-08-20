498d45e2434b9555487acf7a9808746e34508bd8
GeorgioSav <GeorgioSav@yandex.ru>
2023-08-20 22:14:20 +0300

    HomeWork!

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //Task 1
+        System.out.println("Task 1");
+
+
+        //Task 2
+        System.out.println("Task 2");
+
+
+        //Task 3
+        System.out.println("Task 3");
+
+
+        //Task 4
+        System.out.println("Task 4");
+
+
+        //Task 5
+        System.out.println("Task 5");
+
+
+        //Task 6
+        System.out.println("Task 6");
+
+
+        //Task 7
+        System.out.println("Task 7");
+
+
+        //Task 8
+        System.out.println("Task 8");
+
+    }
+}
